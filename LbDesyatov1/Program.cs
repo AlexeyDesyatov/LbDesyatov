2d6536d24b9780c1091206f8958a0ed8e573ebe9
AlexeyDesyatov <avd55@tpu.ru>
2025-09-17 13:57:21 +0700

    Lb6.2.2

--- LbDesyatov1/Program.cs
+++ LbDesyatov1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LbDesyatov1
@@ -10,7 +11,22 @@ namespace LbDesyatov1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine($"Напишите текст: ");
+            string text = Console.ReadLine();
+
+
+            Regex regex = new Regex(@"\bне\b");
+
+            // Находим все совпадения
+            MatchCollection matches = regex.Matches(text);
+
+            // Выводим количество вхождений
+            Console.WriteLine($"Количество вхождений предлога \"не\": {matches.Count}");
+
+
+            Console.Read();
+
         }
+
     }
 }
